030d2a6de08a976264e6db075a5d3758e6825fbf
Crystallen1 <yunchen6@student.unimelb.edu.au>
2024-12-30 15:56:41 +0800

    识别全部歌曲

--- export.py
+++ export.py
@@ -1,26 +1,37 @@
 import re
 import json
+import requests
 
 # 读取 HTML 文件
-with open("data.html", "r", encoding="utf-8") as file:
-    html_content = file.read()
+url = input("请输入要获取 HTML 的 URL: ")
+    # 使用 GET 方法获取 HTML 内容
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
+response = requests.get(url,headers=headers)
+response.raise_for_status()  # 检查请求是否成功
+print(response)
+html_content = response.text
 
 # 匹配包含 JSON 的部分
-pattern = r"window\.__INITIAL_DATA__\s*=\s*({[\s\S]*})"
+pattern = r'var firstPageData = \s*({[\s\S]*?"taogeData":\s*{[\s\S]*?}}});'
 match = re.search(pattern, html_content)
 
 if match:
-    json_text = match.group(1)  # 提取 JSON 文本
-
+    json_text = match.group(0)  # 提取 JSON 文本
+    json_text2 = json_text.strip().lstrip('var firstPageData = ').rstrip(';')
     # 预处理 JSON 文本
-    json_text = json_text.replace("undefined", "null")  # 替换 undefined 为 null
-    json_text = json_text.replace(r"\\u002F", "/")  # 替换转义的斜杠为标准斜杠
+    # json_text = json_text.replace("undefined", "null")  # 替换 undefined 为 null
+    # json_text = json_text.replace(r"\\u002F", "/")  # 替换转义的斜杠为标准斜杠
 
     try:
         # 加载 JSON 数据
-        data = json.loads(json_text)
+        data = json.loads(json_text2)
+        print(data)
         # 从 JSON 数据中提取 songList
-        song_list = data.get('songList', [])
+        data_2 = data.get("taogeData")
+        song_list = data_2.get('songlist', [])
+        print(song_list)
 
         # 解析所需的数据并格式化输出
         for index,song in enumerate(song_list, start=1):
